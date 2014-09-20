f1536981f591e56365b2d623964ad8392391e36f
Vishal Telangre <vishal@weboniselab.com>
2014-09-20 19:53:14 +0530

    Added devise & listed projects deployment access to each user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   validates_presence_of :login, :email
+
+  devise :database_authenticatable, :token_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  alias_attribute :encrypted_password, :crypted_password
+  alias_attribute :password_salt, :salt
+
+  has_and_belongs_to_many :projects
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  devise_for :users
+
+  resources :users, only: [:index]
+  resources :projects, only: [:index]
 
   get 'dashboards/show'
 
-  resources :users, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
